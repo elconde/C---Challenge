026b1d01e90ebb99abbdab26cc26ab377caafa23
Robert Conde <robert.conde@fidessa.com>
2012-03-15 11:26:07 -0400

    Working, but not random

---
+++ guessing/guessing.cc
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main() { 
+    int number=(rand() % 100) + 1;
+    int guess=-1;
+    int trycount=0;
+    while(guess != number && trycount<8) {
+        cout<<"Please enter a guess: "; 
+        cin>>guess; 
+        if(guess<number) {
+            cout<<"Too low"<<endl;
+        }
+        else if(guess>number) {
+            cout<<"Too high"<<endl;
+        }
+        else {
+            cout<<"You guessed the number" << endl;
+            return 0;
+        }
+        trycount++;
+    }
+    printf("Sorry, the number was %d\n",number);
+    return 0; 
+}
+
